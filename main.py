30a29a26c3ac2f6af38a28c4b1ed585d1a3f7eca
Ice Wolfy <icebluewolf05@gmail.com>
2020-09-22 14:40:33 -0500

    Added Basic Functionality For Whitelist Feature
    Signed-off-by: Ice Wolfy <icebluewolf05@gmail.com>

--- main.py
+++ main.py
@@ -1,34 +1,81 @@
 import discord
-import poll
+import poll_function
 import ClientSecret
+import psycopg2
+from discord.ext import commands
+from pydactyl import PterodactylClient
 
 
-client = discord.Client()
+discordClient = commands.Bot(command_prefix="w")
+MCClient = PterodactylClient("https://panel.primedhosting.com/", ClientSecret.PH_API_KEY)
+DB_conn = psycopg2.connect("dbname=Wolfy-Bot user=" + ClientSecret.DBuser + " password=" + ClientSecret.DBpassword)
+
+cur = DB_conn.cursor()
+srv_id = "bf9213e8"
 
 
-@client.event
+@discordClient.event
 async def on_ready():
     print("READY!")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+
+@discordClient.command()
+async def ping(ctx):
+    """
+    Pings The Bots Server
+    """
+
+    await ctx.send("Pong :ping_pong:\n`Ping : " + str(discordClient.latency) + "`")
+
+
+@discordClient.command()
+async def poll(ctx, *, content):
+    await poll_function.poll(ctx, content)
+
+
+@discordClient.command()
+async def whitelist(ctx, *, content):
+    """Allows A User To Whitelist Themselves On The Connected MC Server"""
+    if content.strip == "":
+        print("cancelled")
         return
-    if message.content.startswith("wpoll"):
-        await poll.poll(message, message.author)
-    if message.content.startswith("O.o"):
-        if str(message.author) == "AlphDelta#5641":
-            await message.channel.send("Hehe " + str(message.author) + " STINKY <:SusOwl:715254309079613543> ")
-        else:
-            await message.channel.send("hehe <:SusOwl:715254309079613543>")
-    if message.content.startswith("werf"):
-        await message.channel.send("werf")
-    if message.content.startswith("warf"):
-        await message.channel.send("warf")
+    cur.execute("select count(*) from used_command where user_id = %s", (str(ctx.author.id),))
+    is_present = cur.fetchone()[0]
+    print(is_present)
+    print(ctx.author.id)
+    if is_present > 0 and ctx.author.id != 451848182327148554:
+        await ctx.send("You Have Already Whitelisted An Account.\n"
+                       "If You Changed Your Account Name Or Got A New Account Please Contact A Staff Member.")
+        return
+    else:
+        cur.execute(
+            "INSERT INTO used_command (discord_user, user_id, whitelist) "
+            "VALUES ('{}', {}, True);".format(ctx.author.name + "#" + ctx.author.discriminator, ctx.author.id))
+        DB_conn.commit()
+        # MCClient.client.send_console_command(srv_id, "whitelist add " + content)
+        await ctx.send("The User {} Has Been Added To The Whitelist".format(content))
 
-#@client.event
-#async def on_reaction_add(reaction, user):
-    #if str(user) == "Ice Wolfy#5283":
-        #await reaction.message.channel.send("{} has added {} to the message: {}".format(user.name, reaction.emoji, reaction.message.content))
 
-client.run(ClientSecret.clientSecret)
+@whitelist.error
+async def whitelist_error(ctx, error):
+    print(error)
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Put Your MC Username After The Command To Whitelist Yourself On The MC Server")
+
+# async def on_message(message):
+#     if message.author == discordClient.user:
+#         return
+#     if message.content.startswith("wpoll"):
+#         await poll.poll(message, message.author)
+#     if message.content.startswith("O.o"):
+#         if str(message.author.id) == 521367820957777923 or 451848182327148554:
+#             await message.channel.send("Hehe " + str(message.author)[:-5] + " STINKY <:SusOwl:715254309079613543> ")
+#         else:
+#             await message.channel.send("hehe <:SusOwl:715254309079613543>")
+#     if message.content.startswith("werf"):
+#         await message.channel.send("werf")
+#     if message.content.startswith("warf"):
+#         await message.channel.send("warf")
+
+
+discordClient.run(ClientSecret.clientSecret)
