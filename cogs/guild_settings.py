e52c175fe2139800dd3b6f15393edbd3276d5e4c
Ice Wolfy <icebluewolf05@gmail.com>
2022-01-18 07:58:07 -0600

    Old MySQL Notation -> Postgres
    Signed-off-by: Ice Wolfy <icebluewolf05@gmail.com>

--- cogs/guild_settings.py
+++ cogs/guild_settings.py
@@ -78,24 +78,27 @@ class GuildSettings(commands.Cog):
     async def set(self, ctx, setting, *, value):
         """
         Allows Server Admins To Change The Settings Of The Bot.
-        Usage: `w!set <setting> <value>`
+        Usage: `w!set <setting>.<value>`
         """
 
         setting = setting.split(".", 1)
         if setting[0] in SETTINGS.keys():
-            if setting[1] in SETTINGS[setting[0]]:
-                # setting_type = SETTINGS[setting[0]][setting[1]]['type']
-                # if not exec(f"GuildSettings.is_{setting_type}({ctx}, {value})"):
-                #     return await ctx.send("That Value Did Not Match The Type Of That Setting")
-                value = ascii(value)
-                await self.bot.db.execute("""
-                INSERT INTO guild_settings (guild_id, setting_name, setting_value)
-                VALUES ($1, $2, $3)
-                ON DUPLICATE KEY UPDATE
-                setting_value = $4;""", ctx.guild.id, f"{setting[0]}.{setting[1]}", value, value)
-                await ctx.send(f"Changed The `{setting[0]}.{setting[1]}` Setting To \n`{value}`")
-            else:
-                await ctx.send(f"I Could not Find The `{setting[1]}` Setting In The `{setting[0]}` Group")
+            try:
+                if setting[1] in SETTINGS[setting[0]]:
+                    # setting_type = SETTINGS[setting[0]][setting[1]]['type']
+                    # if not exec(f"GuildSettings.is_{setting_type}({ctx}, {value})"):
+                    #     return await ctx.send("That Value Did Not Match The Type Of That Setting")
+                    value = ascii(value)
+                    await self.bot.db.execute("""
+                    INSERT INTO guild_settings (guild_id, setting_name, setting_value)
+                    VALUES ($1, $2, $3)
+                    ON CONFLICT ON CONSTRAINT guild_settings_pkey DO UPDATE
+                    SET setting_value = $4;""", ctx.guild.id, f"{setting[0]}.{setting[1]}", value, value)
+                    await ctx.send(f"Changed The `{setting[0]}.{setting[1]}` Setting To \n`{value}`")
+                else:
+                    await ctx.send(f"I Could not Find The `{setting[1]}` Setting In The `{setting[0]}` Group")
+            except IndexError:
+                await ctx.send("Make Sure To Use `w!set <setting>.<value>` (Without The `<>`)")
         else:
             await ctx.send(f"I could not find the `{setting[0]}` group")
 
